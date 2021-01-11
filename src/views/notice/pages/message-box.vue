04d26ba4343a0001adac8b964f99f9bdaf68d0f3
zhouhao15 <zhouhao15@baidu.com>
2021-01-11 14:25:06 +0800

    feature: 更新 message-box 样式

--- src/views/notice/pages/message-box.vue
+++ src/views/notice/pages/message-box.vue
@@ -4,6 +4,10 @@
             <p slot="header">确认消息</p>
             <el-button type="text" @click="open">点击打开 Message Box</el-button>
         </el-card>
+        <el-card class="card-item">
+            <p slot="header">居中布局</p>
+            <el-button type="text" @click="open2">点击打开 Message Box</el-button>
+        </el-card>
     </div>
 </template>
 
@@ -13,10 +17,31 @@ import {Component, Vue} from 'vue-property-decorator';
 @Component
 export default class ButtonPage extends Vue {
     open() {
-        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        this.$confirm('您确定要退出吗?', '提示', {
             confirmButtonText: '确定',
             cancelButtonText: '取消',
             type: 'warning'
+        })
+            .then(() => {
+                this.$message({
+                    type: 'success',
+                    message: '删除成功!'
+                });
+            })
+            .catch(() => {
+                this.$message({
+                    type: 'info',
+                    message: '已取消删除'
+                });
+            });
+    }
+
+    open2() {
+        this.$confirm('您确定要退出登录吗?', '提示', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning',
+            center: true
         })
             .then(() => {
                 this.$message({
