67e7b00264b8aa56d61890d31a9c73c4cfae3d8b
zhouhao15 <zhouhao15@baidu.com>
2021-01-15 13:12:03 +0800

    feature: 维护 robo-ui 组件

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * Created by zh on 2021/1/15.
+ */
+import 'lib/index.css';
+
+import Robo403404 from '@/components/robo-403-404/index';
+import RoboAsideNav from '@/components/robo-aside-nav/index';
+import RoboAsideNavMenu from '@/components/robo-aside-nav-menu/index';
+import RoboBreadcrumb from '@/components/robo-breadcrumb/index';
+import RoboOverflowText from '@/components/robo-overflow-text/index';
+import RoboSelectAll from '@/components/robo-select-all/index';
+import RoboSelectMultiCheckbox from '@/components/robo-select-multi-checkbox/index';
+import RoboSelectMultiComplex from '@/components/robo-select-multi-complex/index';
+import RoboSelectMultiSimple from '@/components/robo-select-multi-simple/index';
+import RoboSymbolIcon from '@/components/robo-symbol-icon/index';
+
+export default {
+    Robo403404,
+    RoboAsideNav,
+    RoboAsideNavMenu,
+    RoboBreadcrumb,
+    RoboOverflowText,
+    RoboSelectAll,
+    RoboSelectMultiCheckbox,
+    RoboSelectMultiComplex,
+    RoboSelectMultiSimple,
+    RoboSymbolIcon
+};
