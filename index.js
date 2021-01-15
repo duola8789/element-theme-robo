748bdbbe0cca4003e1c06d71a16fe0c240a404b3
zhouhao15 <zhouhao15@baidu.com>
2021-01-15 13:44:35 +0800

    feature: 组件导出

--- index.js
+++ index.js
@@ -3,16 +3,16 @@
  */
 import 'lib/index.css';
 
-import Robo403404 from '@/components/robo-403-404/index';
-import RoboAsideNav from '@/components/robo-aside-nav/index';
-import RoboAsideNavMenu from '@/components/robo-aside-nav-menu/index';
-import RoboBreadcrumb from '@/components/robo-breadcrumb/index';
-import RoboOverflowText from '@/components/robo-overflow-text/index';
-import RoboSelectAll from '@/components/robo-select-all/index';
-import RoboSelectMultiCheckbox from '@/components/robo-select-multi-checkbox/index';
-import RoboSelectMultiComplex from '@/components/robo-select-multi-complex/index';
-import RoboSelectMultiSimple from '@/components/robo-select-multi-simple/index';
-import RoboSymbolIcon from '@/components/robo-symbol-icon/index';
+import Robo403404 from './src/components/robo-403-404/index';
+import RoboAsideNav from './src/components/robo-aside-nav/index';
+import RoboAsideNavMenu from './src/components/robo-aside-nav-menu/index';
+import RoboBreadcrumb from './src/components/robo-breadcrumb/index';
+import RoboOverflowText from './src/components/robo-overflow-text/index';
+import RoboSelectAll from './src/components/robo-select-all/index';
+import RoboSelectMultiCheckbox from './src/components/robo-select-multi-checkbox/index';
+import RoboSelectMultiComplex from './src/components/robo-select-multi-complex/index';
+import RoboSelectMultiSimple from './src/components/robo-select-multi-simple/index';
+import RoboSymbolIcon from './src/components/robo-symbol-icon/index';
 
 export default {
     Robo403404,
